3e297d91c0d9a437e46c75d550bddf97753e59cf
Brian Whelan <whelanb7@tcd.ie>
2021-10-17 15:16:50 +0100

    update bean randomisation logic

--- beans.asm
+++ beans.asm
@@ -1,23 +1,22 @@
 SECTION "Beans", ROM0
-BEAN_POOL_SIZE EQU $08 ; The number of beans to initialise into the pool
+BEAN_POOL_SIZE EQU $01 ; The number of beans to initialise into the pool
 
 init_beans:
     ld hl, $C14C
     xor a
-    ;ld [FRAME], a
+    ld [FRAME], a
 
 .repeat ; Initialise the falling beans in VRAM
     ld d, a ; cache the loop index in d so we can perform arithmetic on a
     sla a
     sla a
     sla a ; multiply the index by 8
-    sla a
     ldi [hl], a ; zero out the y-pos and increment
     ld a, d ; restore the index into a
     sla a
     sla a
     sla a ; multiply the index by 8
-    add a, $0F ; add $0F, since x pos is -8 and there are walls at the side of the map
+    add a, $10; add $10, since x pos is -8 and there are walls at the side of the map
     ldi [hl], a
     ld a, $69
     ldi [hl], a ; Set sprite to tile 69
@@ -29,78 +28,110 @@ init_beans:
     jr nz, .repeat
     ret ; Otherwise, return
     
-;RandomNumber: ; Fetch a random number from the RandTable;
-
-;        push    hl
-;        ld      a,[RandomPtr]
-;        inc     a
-;        ld      [RandomPtr],a
-;        ld      hl,RandTable
-;        add     a,l
-;        ld      l,a
-;        jr      nc,.skip
-;        inc     h
-;.skip:  ld      a,[hl]
-;        pop     hl
-;        ret
+GetNextBeanAisle: ; Fetch a random number from the Bean Random Number Table;
+        push    hl
+        ld      a, [RandomPtr]
+        inc     a
+        ld      [RandomPtr], a
+        ld      hl, BeanRandomTable
+        add     a, l
+        ld      l, a
+        jr      nc, .skip
+        inc     h
+.skip:  ld      a, [hl]
+        pop     hl
+        ret
 
 
 update_beans:
     xor a ; zero out a
     ld hl, $C14C ; Load the echo OAM address into hl
     ; temporarily remove FRAME counter as it shared a memory address with the x-pos of the second bean (oops!)
-    ;ld a, [FRAME]
-    ;inc a
-    ;ld [FRAME], a ; Increment the frame counter
-    ;SRL a ; 
-    ;jr c, .stop
-    ;ld [FRAME], a
+    ld a, [FRAME]
+    inc a
+    ld [FRAME], a ; Increment the frame counter
+    SRL a ; 
+    jp c, .stop
     xor a
 
 .repeatstuff
-    ld d, a ; load the index into d
+    ld d, a ; load the index into d so we can perform other operations over a
     ld a, [TONGUE_LENGTH] ; Load tongue length
     cp $00 ; check if tongue length is zero
-    jr z, .skipcoltest ; if it is, don't bother checking the beans for tongue collisions
+    jr z, .skipcoltest ; if it is, don't bother checking the beans for tongue collisions, since it isn't deployed
     ld a, [TONGUE_EATING] ; if the tongue is currently eating a bean
     cp $01
     jr z, .skipcoltest ; don't check other beans for collisions, since the tongue will retract
-    push hl ; save hl
+
+
+
+    ; CHECK OVERLAP WITH TONGUE
+    push hl ; save hl, since it is used as a memory address
     ld a, [hl] ; load the current bean y-pos
     ld c, a ; into c
-    inc hl ; inc hl to get x pos
+    inc hl ; inc hl to get x-pos
     ld a, [hl] ; load the current bean x-pos
     ld b, a ; into b
-    ld a, [TONGUE_X] ; load the tongue tip x into h
-    ld h, a
-    ld a, [TONGUE_Y] ; load the tongue tip y into l
-    ld l, a
-    call checkOverlap ; check if bc overlaps with hl
-    ld [TONGUE_EATING], a ; if it is, set the tongue to eating
+    ld a, [TONGUE_X] ; load the tongue tip x-pos
+    ld h, a ; into h
+    ld a, [TONGUE_Y] ; load the tongue tip y-pos
+    ld l, a ; into l
+    call checkOverlap ; check if bc overlaps with hl (if the tongue overlaps with the current bean)
+    ld [TONGUE_EATING], a ; if it is, set the tongue to eating so it knows to retract
     pop hl ; restore hl
     cp $00 ; if the result of checkOverlap was 0
     jr z, .skipcoltest ; skip the next section
-    xor a ; zero out a
-    ld [hl], a ; load zero into hl
+    xor a
+    ld [hl], a ; load zero into location at hl (resets the y-pos of the current bean)
+    inc hl ; REMOVE ME
+    call GetNextBeanAisle
+    sla a
+    sla a
+    sla a
+    ld [hl], a
+    dec hl
     ld a, [GAME_SCORE] ; load the current score into a
     inc a ; increment a
-    daa
+    daa ; convert to binary-coded decimal
     ld [GAME_SCORE], a ; set the current game score to a
 .skipcoltest
+    ; CHECK OVERLAP WITH PLAYER
+    push hl ; save hl, since it is used as a memory address
+    ld a, [hl] ; load the current bean y-pos
+    ld c, a ; into c
+    inc hl ; inc hl to get x-pos
+    ld a, [hl] ; load the current bean x-pos
+    ld b, a ; into b
+    ld h, b
+    ld l, c
+    ld a, [PLAYER_X] ; load the tongue tip x-pos
+    ld b, a ; into h
+    ld a, [PLAYER_Y] ; load the tongue tip y-pos
+    ld c, a ; into l
+    call checkOverlap ; check if bc overlaps with hl (if the tongue overlaps with the current bean)
+    ;ld [GAME_LEVEL], a ; if it is, set the tongue to eating so it knows to retract
+    pop hl ; restore hl
+
+
     ld a, [hl] ; Load the Y-Pos into a
     inc a ; Add one to the Y-Pos
     ld [hl], a ; Restore the Y-Pos
     cp $87 ; $87 is the floor pos
-    jr nz, .dontDestroy
-    xor a
-    ld [hl], a
-    push hl
+    jr nz, .dontDestroy ; if we are not at the floor pos, skip to dontDestroy
+    ld a, [NEXT_BEAN]
+    sla a
+    ld c, a
+    xor a ; otherwise, xor a
+    sub a, c
+    ld [hl], a ; set the y-pos back to 0
+    push hl ; push hl on to the stack
     inc hl
     ld a, [hl] ; Load the X-Pos
     ld hl, $9A00
     srl a
     srl a
-    srl a ; Div by 8
+    srl a ; div by 8
+    sub $01 ; subtract 1 to get correct block
     ld c, a
     xor a
     ld b, a
@@ -114,6 +145,13 @@ update_beans:
     ld [rNR44], a ; Play noise
 .dontReplace
     pop hl
+    inc hl
+    call GetNextBeanAisle
+    sla a
+    sla a
+    sla a
+    ld [hl], a
+    dec hl
 .dontDestroy
     inc hl ; Skip over Y-Pos
     inc hl ; Skip over X-Pos
@@ -122,13 +160,14 @@ update_beans:
     ld a, d ; Restore a
     inc a ; Go to next bean
     cp BEAN_POOL_SIZE ; If we've not updated all beans
-    jr nz, .repeatstuff ; Go to the next bean
+    jp nz, .repeatstuff ; Go to the next bean
 .stop
     ret
 
 
-SECTION "Echo OAM BEANS", WRAM0[$C14C]
-BEAN_Y: DS 1
-BEAN_X: DS 1
-BEAN_SPRITE: DS 1
-BEAN_ATTRIBUTE : DS 1
+SECTION "Echo OAM BEANS", WRAM0[$C14C] ; Bean instances
+BEAN_Y: DS 1 ; First bean instance Y-pos (useful for indexing)
+BEAN_X: DS 1 ; First bean instance X-pos (useful for indexing)
+BEAN_SPRITE: DS 1 ; First bean instance sprite
+BEAN_ATTRIBUTE : DS 1 ; First bean instance sprite attributes
+BEAN_POOL_MEM_CHUNK : DS BEAN_POOL_SIZE * 4 ; Block out enough memory for remaining bean instances (pool size is compile time)
